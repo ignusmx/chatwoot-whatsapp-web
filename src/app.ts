2aa36042154e0d165b895fea0ecfe1d33b8f60f5
Masterkitano <tprado@ignus.mx>
2022-09-15 11:32:20 -0500

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -169,79 +169,79 @@ expressApp.post("/chatwootMessage", async (req, res) => {
 
         //quick authentication with chatwoot api key
         if(apiKey != process.env.CHATWOOT_API_KEY){
-            res.status(400);
+            res.status(401).json({result:"Unauthorized access. Please provide a valid api_access_token."});
+            return;
         }
-        else{
-            const whatsappWebClientState = await whatsappWebClient.getState();
-            //post to whatsapp only if we are connected to the client and message is not private
-            if (whatsappWebClientState === "CONNECTED" 
-                && chatwootMessage.inbox.id == process.env.CHATWOOT_WW_INBOX_ID
-                && chatwootMessage.message_type == "outgoing" 
-                && !chatwootMessage.private) {
-                const chatwootContact = await chatwootAPI.getChatwootContactById(chatwootMessage.conversation.contact_inbox.contact_id);
-                const messages = await chatwootAPI.getChatwootConversationMessages(chatwootMessage.conversation.id);
-                const messageData = messages.find((message:any) => {
-                    return message.id === chatwootMessage.id;
-                });
-                
-                const to = `${chatwootContact.identifier}`;
-                let formattedMessage:string | undefined = chatwootMessage.content;
-                let messageContent:MessageContent | undefined;
-    
-                const chatwootMentions:RegExpMatchArray | null = formattedMessage == null ? null : formattedMessage.match(/@("[^@"']+"|'[^@"']+'|[^@\s"']+)/g);
-                const options:any = {};
-                
-                if(formattedMessage != null && chatwootMentions != null){
-                    const whatsappMentions:Array<Contact> = [];
-                    const groupChat:GroupChat = await whatsappWebClient.getChatById(to) as GroupChat;
-                    const groupParticipants:Array<GroupParticipant> = groupChat.participants;
-                    for (const mention of chatwootMentions) {
-                        for(const participant of groupParticipants){
-                            const mentionIdentifier = mention.substring(1).replaceAll("+","").replaceAll("\"","").replaceAll("'","").toLowerCase();
-                            const participantIdentifier = `${participant.id.user}@${participant.id.server}`;
-                            const contact:Contact = await whatsappWebClient.getContactById(participantIdentifier);
-                            if((contact.name != null && contact.name.toLowerCase().includes(mentionIdentifier)) 
-                            || (contact.pushname != null && contact.pushname.toLowerCase().includes(mentionIdentifier))
-                            || contact.number.includes(mentionIdentifier)){
-                                whatsappMentions.push(contact);
-                                formattedMessage = formattedMessage.replace(mention, `@${participant.id.user}`);
-                                break; //we continue with next mention since we found our contact and there's no need to keep searching
-                            }
+        
+        const whatsappWebClientState = await whatsappWebClient.getState();
+        //post to whatsapp only if we are connected to the client and message is not private
+        if (whatsappWebClientState === "CONNECTED" 
+            && chatwootMessage.inbox.id == process.env.CHATWOOT_WW_INBOX_ID
+            && chatwootMessage.message_type == "outgoing" 
+            && !chatwootMessage.private) {
+            const chatwootContact = await chatwootAPI.getChatwootContactById(chatwootMessage.conversation.contact_inbox.contact_id);
+            const messages = await chatwootAPI.getChatwootConversationMessages(chatwootMessage.conversation.id);
+            const messageData = messages.find((message:any) => {
+                return message.id === chatwootMessage.id;
+            });
+            
+            const to = `${chatwootContact.identifier}`;
+            let formattedMessage:string | undefined = chatwootMessage.content;
+            let messageContent:MessageContent | undefined;
+
+            const chatwootMentions:RegExpMatchArray | null = formattedMessage == null ? null : formattedMessage.match(/@("[^@"']+"|'[^@"']+'|[^@\s"']+)/g);
+            const options:any = {};
+            
+            if(formattedMessage != null && chatwootMentions != null){
+                const whatsappMentions:Array<Contact> = [];
+                const groupChat:GroupChat = await whatsappWebClient.getChatById(to) as GroupChat;
+                const groupParticipants:Array<GroupParticipant> = groupChat.participants;
+                for (const mention of chatwootMentions) {
+                    for(const participant of groupParticipants){
+                        const mentionIdentifier = mention.substring(1).replaceAll("+","").replaceAll("\"","").replaceAll("'","").toLowerCase();
+                        const participantIdentifier = `${participant.id.user}@${participant.id.server}`;
+                        const contact:Contact = await whatsappWebClient.getContactById(participantIdentifier);
+                        if((contact.name != null && contact.name.toLowerCase().includes(mentionIdentifier)) 
+                        || (contact.pushname != null && contact.pushname.toLowerCase().includes(mentionIdentifier))
+                        || contact.number.includes(mentionIdentifier)){
+                            whatsappMentions.push(contact);
+                            formattedMessage = formattedMessage.replace(mention, `@${participant.id.user}`);
+                            break; //we continue with next mention since we found our contact and there's no need to keep searching
                         }
                     }
-                    options.mentions = whatsappMentions;
                 }
-    
-                if(process.env.PREFIX_AGENT_NAME_ON_MESSAGES == "true")
-                {
-                    formattedMessage = `${chatwootMessage.sender?.name}: ${formattedMessage??""}`;
-                }
-                
-                if(messageData.attachments != null && messageData.attachments.length > 0)
-                {
-                    const media = await MessageMedia.fromUrl(messageData.attachments[0].data_url);
-                    if(formattedMessage != null)
-                    {
-                        options.caption = formattedMessage;
-                    }
-    
-                    messageContent = media;
-                }
-                else
-                {
-                    messageContent = formattedMessage;
-                }
-    
-                if(messageContent != null)
-                {
-                    whatsappWebClient.sendMessage(to, messageContent, options);
-                }   
+                options.mentions = whatsappMentions;
             }
-    
-            res.status(200).json({ result: "message_sent_succesfully" });   
+
+            if(process.env.PREFIX_AGENT_NAME_ON_MESSAGES == "true")
+            {
+                formattedMessage = `${chatwootMessage.sender?.name}: ${formattedMessage??""}`;
+            }
+            
+            if(messageData.attachments != null && messageData.attachments.length > 0)
+            {
+                const media = await MessageMedia.fromUrl(messageData.attachments[0].data_url);
+                if(formattedMessage != null)
+                {
+                    options.caption = formattedMessage;
+                }
+
+                messageContent = media;
+            }
+            else
+            {
+                messageContent = formattedMessage;
+            }
+
+            if(messageContent != null)
+            {
+                whatsappWebClient.sendMessage(to, messageContent, options);
+            }   
         }
+
+        res.status(200).json({ result: "message_sent_succesfully" });   
     } catch {
-        res.status(400);
+        res.status(400).json({result:"exception_error"});
     }
 });
 
